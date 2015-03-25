24209b8149368f996eaaadc6137276d049822fdb
Edwin Estrada <edwin@thinkpixbit.com>
2015-03-25 09:23:27 -0400

    Header portion of site only on front page

--- wp-content/themes/theonepager/header.php
+++ wp-content/themes/theonepager/header.php
@@ -8,13 +8,13 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  * @package WooFramework
  * @subpackage Template
  */
- 
+
  global $woo_options, $woocommerce;
 
  $settings = array(
-	'header_content' => ''
+  'header_content' => ''
  );
-	
+
  $settings = woo_get_dynamic_values( $settings );
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
@@ -31,31 +31,33 @@ woo_head();
 <body <?php body_class(); ?>>
 <?php woo_top(); ?>
 <div id="wrapper">
-    
-    <?php woo_header_before(); ?>
 
-	<header id="header">
-		<div class="col-full">
-		
-			<?php woo_header_inside(); ?>
-	    	
-	    	<div id="hgroup">
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-			</div>
-			
-			<?php if ( $settings['header_content'] != '' ) { ?>
-				<p><?php echo do_shortcode( stripslashes( $settings['header_content'] ) ); ?></p>
-			<?php } ?>
-			
-			<?php if ( is_woocommerce_activated() && isset( $woo_options['woocommerce_header_cart_link'] ) && 'true' == $woo_options['woocommerce_header_cart_link'] ) { ?>
-		    	<ul class="nav cart fr">
-		    		<li>		<a class="cart-contents" href="<?php echo $woocommerce->cart->get_cart_url(); ?>" title="<?php _e('View your shopping cart', 'woothemes'); ?>"><?php echo $woocommerce->cart->get_cart_total(); ?> <span class="count"><?php echo sprintf( _n('%d item', '%d items', $woocommerce->cart->get_cart_contents_count(), 'woothemes' ), $woocommerce->cart->get_cart_contents_count() );?></span></a>
+  <?php woo_header_before(); ?>
+
+  <?php if(is_front_page()): ?>
+  <header id="header">
+    <div class="col-full">
+
+      <?php woo_header_inside(); ?>
+
+        <div id="hgroup">
+        <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+        <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+      </div>
+
+      <?php if ( $settings['header_content'] != '' ) { ?>
+        <p><?php echo do_shortcode( stripslashes( $settings['header_content'] ) ); ?></p>
+      <?php } ?>
+
+      <?php if ( is_woocommerce_activated() && isset( $woo_options['woocommerce_header_cart_link'] ) && 'true' == $woo_options['woocommerce_header_cart_link'] ) { ?>
+          <ul class="nav cart fr">
+            <li>    <a class="cart-contents" href="<?php echo $woocommerce->cart->get_cart_url(); ?>" title="<?php _e('View your shopping cart', 'woothemes'); ?>"><?php echo $woocommerce->cart->get_cart_total(); ?> <span class="count"><?php echo sprintf( _n('%d item', '%d items', $woocommerce->cart->get_cart_contents_count(), 'woothemes' ), $woocommerce->cart->get_cart_contents_count() );?></span></a>
 </li>
-		   		</ul>
-	    	<?php } ?>
-	        
-		</div>
-	</header><!-- /#header -->
+           </ul>
+        <?php } ?>
 
-	<?php woo_content_before(); ?>
+    </div>
+  </header><!-- /#header -->
+  <?php endif; ?>
+
+  <?php woo_content_before(); ?>
