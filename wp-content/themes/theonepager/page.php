da2367a5a4b114315f648d81a5f446361d00b0a3
Edwin Estrada <edwin@thinkpixbit.com>
2015-03-25 08:55:07 -0400

    Responsive hiding for schedule

--- wp-content/themes/theonepager/page.php
+++ wp-content/themes/theonepager/page.php
@@ -13,57 +13,57 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 	get_header();
 	global $woo_options;
 ?>
-       
+
     <div id="content" class="page">
-    
+
     	<div class="col-full">
-    
+
     		<?php woo_main_before(); ?>
-    		
-			<section id="main" class="col-left"> 			
-			
+
+			<section id="main" class="col-left">
+
         	<?php
         		if ( have_posts() ) { $count = 0;
         			while ( have_posts() ) { the_post(); $count++;
-        	?>                                                           
+        	?>
         	    <article <?php post_class(); ?>>
-					
+
 					<header>
 				    	<h1><?php the_title(); ?></h1>
 					</header>
-					
+
         	        <section class="entry">
         	        	<?php the_content(); ?>
-			
+
 						<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'woothemes' ), 'after' => '</div>' ) ); ?>
         	       	</section><!-- /.entry -->
-			
+
 					<?php edit_post_link( __( '{ Edit }', 'woothemes' ), '<span class="small">', '</span>' ); ?>
-        	        
+
         	    </article><!-- /.post -->
-        	    
+
         	    <?php
         	    	// Determine wether or not to display comments here, based on "Theme Options".
         	    	if ( isset( $woo_options['woo_comments'] ) && in_array( $woo_options['woo_comments'], array( 'page', 'both' ) ) ) {
         	    		comments_template();
         	    	}
-			
+
 					} // End WHILE Loop
 				} else {
 			?>
 				<article <?php post_class(); ?>>
         	    	<p><?php _e( 'Sorry, no posts matched your criteria.', 'woothemes' ); ?></p>
         	    </article><!-- /.post -->
-        	<?php } // End IF Statement ?>  
-        	
+        	<?php } // End IF Statement ?>
+
 			</section><!-- /#main -->
-			
+
 			<?php woo_main_after(); ?>
-			
+
         	<?php get_sidebar(); ?>
-        
+
         </div><!-- /.col-full -->
 
     </div><!-- /#content -->
-		
+
 <?php get_footer(); ?>
